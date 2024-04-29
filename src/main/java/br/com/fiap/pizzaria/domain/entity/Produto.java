5ea67d45b5d8564f3f67682c0901d13fa07f0527
logonrmlocal <matheuscolossal1803@gmail.com>
2024-04-29 11:54:34 -0300

    Primeiro commit em aula!

--- src/main/java/br/com/fiap/pizzaria/domain/entity/Produto.java
+++ src/main/java/br/com/fiap/pizzaria/domain/entity/Produto.java
@@ -1,5 +1,6 @@
 package br.com.fiap.pizzaria.domain.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,10 +15,54 @@ import java.util.Set;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "TB_PRODUTO")
 public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_PRODUTO")
+    @SequenceGenerator(name = "SQ_PRODUTO", sequenceName = "SQ_PRODUTO", allocationSize = 1)
+    @Column(name = "ID_PRODUTO")
     private Long id;
+
+    @Column(name = "NM_PRODUTO")
     private String nome;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(
+            name = "SABOR",
+            referencedColumnName = "ID_SABOR",
+            foreignKey = @ForeignKey(
+                    name = "FK_SABOR_PRODUTO"
+            )
+    )
     private Sabor sabor;
+
+    @Column(name = "PR_PRODUTO")
     private BigDecimal preco;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinTable(
+            name = "TB_PRODUTO_OPCIONAL",
+            joinColumns = {
+                    @JoinColumn(
+                            name = "PRODUTO",
+                            referencedColumnName = "ID_PRODUTO",
+                            foreignKey = @ForeignKey(
+                                    name = "FK_PRODUTO_OPCIONAL"
+                            )
+                    )
+            },
+            inverseJoinColumns = {
+                    @JoinColumn(
+                            name = "OPCIONAL",
+                            referencedColumnName = "ID_OPCIONAL",
+                            foreignKey = @ForeignKey(
+                                    name = "FK_OPCIONAL_PRODUTO"
+                            )
+                    )
+            }
+    )
+
     private Set<Opcional> opcionais = new LinkedHashSet<>();
 }
